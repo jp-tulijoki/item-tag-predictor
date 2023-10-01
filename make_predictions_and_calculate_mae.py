dc7069a6069eae8a37326ab7bc07cfac30c7eb97
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2023-10-01 09:31:23 +0300

    more secure mae calculation

--- make_predictions_and_calculate_mae.py
+++ make_predictions_and_calculate_mae.py
@@ -8,6 +8,7 @@ warnings.filterwarnings('ignore')
 
 tag_path = 'movie_dataset_public_final/raw/tags.json'
 tenfold_path = 'movie_dataset_public_final/processed/10folds'
+PREDICTION_DF_EXISTS = False
 
 def get_tags():
     with open(tag_path) as file:
@@ -17,7 +18,7 @@ def get_tags():
             tag_dict[json_dict["id"]] = json_dict["tag"]  
         return tag_dict
     
-def create_item_tag_scores_predictions(prediction_file):
+def create_item_tag_scores_predictions(prediction_file, tags):
     prediction_df = pd.read_csv(f"predictions/{prediction_file}")
     tag_id = int(prediction_file.split("_")[-1].split(".")[0])
     tag = tags[tag_id]
@@ -33,38 +34,39 @@ def create_item_tag_scores_predictions(prediction_file):
         item_tag_prediction_df = item_tag_prediction_df.append({'item_id': item_id, 'tag_id': tag_id, 'tag': tag, 'predictions': predictions_array.item()}, ignore_index=True)
     return item_tag_prediction_df     
 
-def make_predictions_and_calculate_mae(combined_prediction_df, tenfold):
-    predictions = []
-    tagDL = pd.read_csv(f"{tenfold_path}/test{tenfold}.csv").rename(columns={"movieId": "item_id"})
-    for row in tagDL.iterrows():
-        tag = row[1]["tag"]
-        item_id = row[1]["item_id"]
-        df_row = combined_prediction_df[(combined_prediction_df["item_id"] == item_id) & (combined_prediction_df["tag"] == tag)]
-        if not df_row.empty:
-            predictions.append(round(df_row["predictions"].item() * 4 + 1, 2))
-        else:
-            raise LookupError(f"Prediction for item {item_id} and tag {tag} not found.")
-    with open(f"processed_data/bert_predictions{tenfold}.txt", "w") as file:
-        file.write("\n".join(map(str, predictions)))
-    targets = tagDL["targets"].to_list()
-    print(f"MAE for set {tenfold}: {mean_absolute_error(predictions, targets)}")                
-        
-prediction_files = sorted(os.listdir("predictions"))
+def make_predictions_for_tenfolds(combined_prediction_df, tenfold):
+    test = pd.read_csv(f"{tenfold_path}/test{tenfold}.csv").rename(columns={"movieId": "item_id"})
+    predictions = test[["tag", "item_id"]].copy()
+    predictions = pd.merge(predictions, combined_prediction_df, on=["tag", "item_id"], how="left")
+    predictions["predictions"] = round(predictions["predictions"] * 4 + 1, 2)
+    predictions.to_csv(f"processed_data/bert_predictions{tenfold}.csv", index=False)
+    return predictions, test
 
-with open ("score_list.txt", "r") as file:
-    score_list = file.read().split("\n")[:-1]  
+def calculate_mae(predictions, test, i):
+    evaluation = pd.merge(test, predictions, on=["tag", "item_id"], how="left")
+    evaluation["error"] = evaluation.targets - evaluation.predictions
+    mae = mean_absolute_error(evaluation.targets, evaluation.predictions)
+    assert mae == evaluation["error"].abs().sum() / len(evaluation)
+    print(f"MAE for set {i}: {mae}")           
 
-combined_prediction_df = pd.DataFrame(columns=['item_id', 'tag_id', 'predictions'])
-tags = get_tags()
+def make_combined_prediction_df():
+    prediction_files = sorted(os.listdir("predictions"))
+    combined_prediction_df = pd.DataFrame(columns=['item_id', 'tag_id', 'predictions'])
+    tags = get_tags()
 
-for i, prediction_file in enumerate(prediction_files):
-    score_file = score_list[i]
-    target = pd.read_csv(f"scores/{score_file}")
-    item_tag_predictions = create_item_tag_scores_predictions(prediction_file)
-    combined_prediction_df = pd.concat([combined_prediction_df, item_tag_predictions], ignore_index=True)
-    combined_prediction_df["item_id"].astype('Int64')
+    for prediction_file in prediction_files:
+        item_tag_predictions = create_item_tag_scores_predictions(prediction_file, tags)
+        combined_prediction_df = pd.concat([combined_prediction_df, item_tag_predictions], ignore_index=True)
+        combined_prediction_df["item_id"].astype('Int64')
 
-combined_prediction_df.to_csv("processed_data/merged_predictions/merged_predictions.csv", index=False)
+    combined_prediction_df.to_csv("processed_data/merged_predictions/merged_predictions.csv", index=False)
+
+if not PREDICTION_DF_EXISTS:
+    make_combined_prediction_df()
+
+combined_prediction_df = pd.read_csv("processed_data/merged_predictions/merged_predictions.csv")
 
 for i in range(10):
-    make_predictions_and_calculate_mae(combined_prediction_df, i)
+    predictions, test = make_predictions_for_tenfolds(combined_prediction_df, i)
+    calculate_mae(predictions, test, i)
+
